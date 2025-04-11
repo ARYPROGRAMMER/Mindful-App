057550862d81f5276dd69980ed18f344f1cde7c7
Arya Pratap Singh <arya.2023ug1104@iiitranchi.ac.in>
2025-04-11 12:19:41 +0530

    Update index.js

--- server/Mental Health api/infrastructure/db-fast/index.js
+++ server/Mental Health api/infrastructure/db-fast/index.js
@@ -2,10 +2,10 @@ const Redis = require('redis');
 
 const redis = Redis.createClient({
     socket: {
-        host: 'redis-15193.c309.us-east-2-1.ec2.redns.redis-cloud.com',
-        port: 15193,
+        host: 'redis-10414.c264.ap-south-1-1.ec2.redns.redis-cloud.com',
+        port: 10414,
     },
-    password: 'qJ3EEzX4aQ0LwiPCeP5sMAUZycRiNOK1'
+    password: 'g4PKzYUBIIhr9fREKkbsJ9R325MyvnOS'
 });
 
 (async () => {
