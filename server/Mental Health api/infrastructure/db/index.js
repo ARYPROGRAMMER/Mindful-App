df331279c7fc4c860abdd5185626d480328be59c
Arya Pratap Singh <arya.2023ug1104@iiitranchi.ac.in>
2025-04-11 13:08:03 +0530

    SSL TLS [skip ci]

--- server/Mental Health api/infrastructure/db/index.js
+++ server/Mental Health api/infrastructure/db/index.js
@@ -2,9 +2,9 @@ const { query } = require('express');
 const {Pool} = require('pg');
 const pool = new Pool({
   connectionString: "postgresql://mental_health_app_primary_db_user:NATFLXr30V4UwD79F1Z9dllAuXK6YzAN@dpg-cvsbg824d50c738cma40-a.oregon-postgres.render.com/mental_health_app_primary_db",
-    // ssl: {
-    //     rejectUnauthorized: false
-    // }
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = {
